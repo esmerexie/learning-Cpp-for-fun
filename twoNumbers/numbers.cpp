509f12b984d32d5966f1612df1b2b4dbd1da18e9
Rexie Esmeralda <esmeraldarexie@gmail.com>
2023-05-09 15:15:31 -0700

    added comments to twoNumbers for better understanding of code

--- twoNumbers/numbers.cpp
+++ twoNumbers/numbers.cpp
@@ -4,10 +4,15 @@ int main()
 {
     std::cout << "Enter two numbers with a space: ";
     
+    // setting the values for variables x & y
     int x = { 0 };
     int y = { 0 };
 
+    // cin = character input
+    // >> = requires two int inputs from the user 
     std::cin >> x >> y;
+
+    //returns input as output and then line breaks.
     std::cout << "You have entered: " << x << " and " << y << '\n';
 
     return 0;
