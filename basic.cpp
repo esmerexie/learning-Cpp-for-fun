73fed6f893b9078585617af2b2ee7bf142ef21b7
Rexie Esmeralda <esmeraldarexie@gmail.com>
2023-05-09 14:40:33 -0700

    pushing first program

---
+++ basic.cpp
@@ -0,0 +1,14 @@
+// essentialy the input and output needed for the conosle and std::cout to run
+#include <iostream>
+
+// every c++ program needs a main function. Everything inside main will be executed
+int main()
+{
+
+// cout = character output.
+// << = sends letters or numbers to the console to be output. 
+    std::cout << "Hello Stranger!";
+
+// all this return does is tells the computer that the program ran successfully if it returns 0
+    return 0;
+}
